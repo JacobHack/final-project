9f6598a97cb7ee18a02bba749f3bb0f486fb372e
JacobHack <60163560+JacobHack@users.noreply.github.com>
2022-05-03 15:11:03 -0700

    Update final.py

--- final.py
+++ final.py
@@ -12,6 +12,7 @@ blues = sns.color_palette("light:b", as_cmap=True)
 
 plt.rcParams['figure.figsize'] = [10, 10]
 plt.rcParams['figure.dpi'] = 200
+plt.style.use("dark_background")
 sns.set_style('whitegrid')
-p = sns.heatmap(corr, cmap=blues)
+p = sns.heatmap(corr, cmap=seaborn.color_palette("mako", as_cmap=True))
 p.get_figure().savefig("medals.png",bbox_inches='tight',transparent=True)
